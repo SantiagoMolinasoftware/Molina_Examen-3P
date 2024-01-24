e7bd4c8ca72525d48d88d8e1053054c4849279ce
Santiago Molina <santiago.molina.olivo@udla.edu.ec>
2024-01-24 15:18:30 -0500

    1er commit: Inicio del programa declaracion de librerias y la estructura para el manejo de archivos

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct producto
+{
+    char codigo[25];
+    char nombreProducto[50];
+    char descripcionProducto[50];
+    int cantidad;
+    float precioCompra;
+};
