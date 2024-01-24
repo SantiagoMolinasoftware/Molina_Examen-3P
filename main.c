9cbfe40368daaa86b020666961abaad651fd7a5f
Santiago Molina <santiago.molina.olivo@udla.edu.ec>
2024-01-24 15:22:53 -0500

    2do commit: creacion de la funcion para agregar productos

--- main.c
+++ main.c
@@ -10,3 +10,24 @@ struct producto
     int cantidad;
     float precioCompra;
 };
+
+//Funcion para agregar productos
+void agregarProducto(struct producto *p, int *n)
+{
+    printf("\nIngrese el codigo del producto: ");
+    scanf("%s", p[*n].codigo);
+
+    printf("\nIngrese el nombre del producto: ");
+    scanf("%s", p[*n].nombreProducto);
+
+    printf("\nIngrese la descripcion del producto: ");
+    scanf("%s", p[*n].descripcionProducto);
+
+    printf("\nIngrese la cantidad del producto: ");
+    scanf("%d", &p[*n].cantidad);
+
+    printf("\nIngrese el precio de compra del producto: ");
+    scanf("%f", &p[*n].precioCompra);
+
+    (*n)++;
+}
